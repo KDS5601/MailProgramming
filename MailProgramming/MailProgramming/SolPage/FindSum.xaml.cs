272aae4f6d2a1f39516f5655743c11fae2f7f7c4
김대성 <daeseong@jumong.kr>
2018-07-31 11:51:14 +0900

    find sum 퀵정렬 추가

--- MailProgramming/MailProgramming/SolPage/FindSum.xaml.cs
+++ MailProgramming/MailProgramming/SolPage/FindSum.xaml.cs
@@ -23,14 +23,71 @@ namespace MailProgramming
             int a = 0, b = 0;
             bool resurt = false;
 
-
+            if (intList_r.Count == 1)
+            {
+                return (false, 0, 0);
+            }
 
             return (resurt, a, b);
         }
 
+        public void QuickSorting(ref List<int> intList_r, int left, int right)
+        {
+            int i = left, j = right;
+            int pivot = intList_r[(left + right) / 2];
+            int temp;
+
+            do
+            {
+                while (intList_r[i] < pivot)
+                    i++;
+                while (intList_r[j] > pivot)
+                    j--;
+                if (i <= j)
+                {
+                    temp = intList_r[i];
+                    intList_r[i] = intList_r[j];
+                    intList_r[j] = temp;
+                    i++;
+                    j--;
+                }
+            } while (i <= j);
+            if (left < j)
+                QuickSorting(ref intList_r, left, j);
+
+            if (i < right)
+                QuickSorting(ref intList_r, i, right);
+        }
+
         public void QuickSorting(ref List<int> intList_r)
         {
+            int left = intList_r[0];
+            int right = intList_r[intList_r.Count - 1];
 
+            int i = left, j = right;
+            int pivot = intList_r[(left + right) / 2];
+            int temp;
+
+            do
+            {
+                while (intList_r[i] < pivot)
+                    i++;
+                while (intList_r[j] > pivot)
+                    j--;
+                if (i <= j)
+                {
+                    temp = intList_r[i];
+                    intList_r[i] = intList_r[j];
+                    intList_r[j] = temp;
+                    i++;
+                    j--;
+                }
+            } while (i <= j);
+            if (left < j)
+                QuickSorting(ref intList_r, left, j);
+
+            if (i < right)
+                QuickSorting(ref intList_r, i, right);
         }
     }
     
