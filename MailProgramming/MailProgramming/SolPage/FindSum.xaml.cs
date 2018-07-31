669c1a288dfa90076f0d99c53998554c8a2a04cb
김대성 <daeseong@jumong.kr>
2018-07-31 11:38:57 +0900

    인덱스 찾기 문제풀이 시작

---
+++ MailProgramming/MailProgramming/SolPage/FindSum.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MailProgramming
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class FindSum : ContentPage
+	{
+		public FindSum ()
+		{
+			InitializeComponent ();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        public (bool resurt, int first, int seceond) FindIndex(ref List<int> intList_r)
+        {
+            int a = 0, b = 0;
+            bool resurt = false;
+
+
+
+            return (resurt, a, b);
+        }
+
+        public void QuickSorting(ref List<int> intList_r)
+        {
+
+        }
+    }
+    
+
+}
